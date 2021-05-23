6fe7f3ea75955a5c3ab457a65b2366a865222fd4
oglejsi2 <oglejsi2@gmail.com>
2021-05-23 17:56:23 +0200

    messagePlease enter the commit message for your changes. Lines starting

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,16 +3,7 @@ import javax.swing.JFrame;
 
 public class HelloWorld {
 
-    public static void main (String[] args) {
-        System.out.println("Hello World");
-        JFrame locJFrame = new JFrame();
-        JButton locJbutton = new JButton("Puseh me");
-        
-        
-
-                
-        locJFrame.add(locJbutton);
-        locJFrame.setVisible(true);   
+    public static void main (String[] args) { 
         System.out.println("End");
 
     }
